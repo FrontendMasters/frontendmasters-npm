40addc21b931f5c516ae4d0171b4fe74b563223e
Marc Grabanski <m@marcgrabanski.com>
2025-11-21 17:10:49 -0600

    publish frontendmasters npm module

---
+++ README.md
@@ -0,0 +1,6 @@
+## Installation
+```bash
+npm install frontendmasters
+```
+
+A surprise will appear in your terminal after installation.
---
+++ index.js
@@ -0,0 +1,1 @@
+export const getDiscount = () => 'https://frontendmasters.com/join/?code=blackfriday_signup&utm_source=fm&utm_medium=terminal&utm_campaign=blackfriday_npm';
